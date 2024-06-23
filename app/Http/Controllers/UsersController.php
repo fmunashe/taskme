51216eee2f1f35124c562a2d614b21a4bc79fb9e
Farai Zihove <farai.zihove@econet.co.zw>
2024-06-23 23:45:40 +0200

    add profile logic

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,7 +14,7 @@ class UsersController extends BaseController
 
     public function index(): JsonResponse
     {
-        $users = User::query()->with('profile')->latest()->paginate(10);
+        $users = User::query()->with(['profile','profile.experiences'])->latest()->paginate(10);
         return $this->buildSuccessResponse($users, "Records retrieved successfully");
     }
 
